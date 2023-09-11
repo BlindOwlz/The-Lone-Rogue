e94afb75e6830d312de66cf32a37d9400babce6b
BlindOwlz <144694578+BlindOwlz@users.noreply.github.com>
2023-09-11 09:12:05 -0400

    Create tlr.py

---
+++ tlr.py
@@ -0,0 +1,1 @@
+#The sotry will begin inside a cave system. The player will be in a state of confusion and pain from an unknown origin (possibly the fall or method that resulted in them ending up in the cave?)
